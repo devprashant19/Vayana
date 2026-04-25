ea9345cb3e4a4fda9dd2941b6ce696f159f3b02b
Hridanshu <hridanshu97@gmail.com>
2026-04-25 13:52:32 +0530

    fix: refactor require to import in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,7 @@
-const {
-    default: flattenColorPalette,
-  } = require("tailwindcss/lib/util/flattenColorPalette");
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
   
   /** @type {import('tailwindcss').Config} */
-  module.exports = {
+  export default {
     content: [
       // your paths
       "./src/**/*.{ts,tsx}",
