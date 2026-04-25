13f606766adf08ebfb930edc6cb56c4fa135a66a
Hridanshu <hridanshu97@gmail.com>
2026-04-25 13:47:56 +0530

    fix: use req and res params in dashboard controller

--- backend/src/controllers/dashboard.controller.js
+++ backend/src/controllers/dashboard.controller.js
@@ -4,11 +4,11 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getUserData = asyncHandler(async (req, res) => {
-   
+    return res.status(200).json(new ApiResponse(200, { user: req.user }, "User data"));
 })
 
 const getUserHistory = asyncHandler(async (req, res) => {
-    
+    return res.status(200).json(new ApiResponse(200, { user: req.user }, "User history"));
 })
 
 export {
